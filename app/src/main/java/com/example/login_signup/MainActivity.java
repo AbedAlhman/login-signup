a3ab0da9519491a173d6df8df960261fdd24457b
mira <mira.fms3@gmail.com>
2023-10-07 09:29:55 +0300

    Initial commit

---
+++ app/src/main/java/com/example/login_signup/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.login_signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etEmail,etPassword,etConfirmPassword;
+    Button btnSingUp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void signup(View view) {
+    }
+}
